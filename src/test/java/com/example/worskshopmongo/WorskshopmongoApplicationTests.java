cd5c9a16265635b936197925391e3c867b3396ce
Junior <juniorn06@gmail.com>
2021-11-17 10:57:57 -0300

    projeto criado

---
+++ src/test/java/com/example/worskshopmongo/WorskshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worskshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorskshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
